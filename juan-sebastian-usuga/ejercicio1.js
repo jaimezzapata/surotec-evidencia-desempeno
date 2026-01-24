894bbc8d5b283b2d6e356115366a8e5003fe9fc8
SebasXPG <juanusugas18@gmail.com>
2026-01-24 13:16:17 -0500

    feat: logica de cajero con billetes 50000 20000 y 10000

---
+++ juan-sebastian-usuga/ejercicio1.js
@@ -0,0 +1,25 @@
+function calcularCajero(monto) {
+  if (monto % 10000 !== 0) {
+    return "Monto invalido";
+  }
+
+  let billetes50000 = Math.floor(monto / 50000);
+  monto = monto % 50000;
+
+  let billetes20000 = Math.floor(monto / 20000);
+  monto = monto % 20000;
+
+  let billetes10000 = Math.floor(monto / 10000);
+
+  return {
+    billetes50000: billetes50000,
+    billetes20000: billetes20000,
+    billetes10000: billetes10000,
+  };
+}
+
+function ejercicio1() {
+  let monto = Number(prompt("Ingrese el monto a retirar"));
+  let resultado = calcularCajero(monto);
+  alert(JSON.stringify(resultado));
+}
