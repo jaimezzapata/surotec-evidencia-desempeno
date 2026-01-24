1a7bfaf5849138778e0839d3955ad10472a5480c
Karina Ospina <karinaospinaospina@gmail.com>
2026-01-24 11:00:11 -0500

    feat: sistema de becas

---
+++ karina-ospina-ospina/ejercicio3.js
@@ -0,0 +1,13 @@
+function ejercicio3(estudiante) {
+  if (estudiante.promedio >= 4.5) return true;
+  
+  if (estudiante.edad > 18 && 
+      (estudiante.estrato === 1 || estudiante.estrato === 2) && 
+      estudiante.promedio >= 4.0) {
+    return true;
+  }
+  
+  return false;
+}
+
+module.exports = { ejercicio3 };
