e541aa09de2b2cfb20f9774387e7b9c5085135d9
Samuel01112105 <samuelalvarezmontoya1@gmail.com>
2026-01-24 09:32:08 -0500

    feat: implementación de lógica para el ejercicio 3 (Sistema de becas: conocer si aplica o no un estudiante - como objeto)

---
+++ samuel-alvarez-montoya/ejercicio3.js
@@ -0,0 +1,15 @@
+function tieneBeca(estudiante) {
+
+    if (estudiante.promedio >= 4.5) {
+        return true;
+    }
+
+    if (
+        estudiante.edad > 18 && (estudiante.estrato === 1 || estudiante.estrato === 2) && estudiante.promedio >= 4.0
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
