c20416c5dad1ebfe9299e28daa001785b9e205b8
Milena Moncada <smilemonkis@gmail.com>
2026-01-30 21:57:55 -0500

    Ejercicio 19: Plan de Ruta de Entrega

---
+++ sandra-milena-moncada-silvera/ejercicio19.js
@@ -0,0 +1,17 @@
+function planificarRuta(destinos) {
+    let combustible = 200;
+    let rutaViable = [];
+
+    for (let i = 0; i < destinos.length; i++) {
+        let tramo = destinos[i];
+
+        if (combustible >= tramo.distancia) {
+            rutaViable.push(tramo);
+            combustible -= tramo.distancia;
+        } else {
+            break;
+        }
+    }
+
+    return rutaViable;
+}
