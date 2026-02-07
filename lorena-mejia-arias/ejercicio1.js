bfa178c4d1e9f81f1cbc0952b7a084746686e56d
Lorena Mejia Arias <lorena.mejia@globant.com>
2026-02-06 22:48:37 -0500

    Modificacion de Index y Menu, feat: Cajero de 50, 20, 10

--- lorena-mejia-arias/ejercicio1.js
+++ lorena-mejia-arias/ejercicio1.js
@@ -1,25 +1,27 @@
-function calcularCajero(monto) {
-  if (monto % 10000 !== 0) {
+function procesarRetiro(valor) {
+  if (valor % 10000 !== 0) {
     return "Monto invalido";
   }
 
-  let billetes50000 = Math.floor(monto / 50000);
-  monto = monto % 50000;
+  let restante = valor;
 
-  let billetes20000 = Math.floor(monto / 20000);
-  monto = monto % 20000;
+  let cantidad50 = Math.floor(restante / 50000);
+  restante -= cantidad50 * 50000;
 
-  let billetes10000 = Math.floor(monto / 10000);
+  let cantidad20 = Math.floor(restante / 20000);
+  restante -= cantidad20 * 20000;
+
+  let cantidad10 = Math.floor(restante / 10000);
 
   return {
-    billetes50000: billetes50000,
-    billetes20000: billetes20000,
-    billetes10000: billetes10000,
+    billetes50000: cantidad50,
+    billetes20000: cantidad20,
+    billetes10000: cantidad10,
   };
 }
 
 export function ejercicio1() {
-  let monto = Number(prompt("Ingrese el monto a retirar"));
-  let resultado = calcularCajero(monto);
+  let valor = Number(prompt("Digite el monto a retirar"));
+  let resultado = procesarRetiro(valor);
   alert(JSON.stringify(resultado));
 }
--- lorena-mejia-arias/menu.js
+++ lorena-mejia-arias/menu.js
@@ -1,107 +1,72 @@
 import { ejercicio1 } from "./ejercicio1.js";
+import { ejercicio2 } from "./ejercicio2.js";
+import { ejercicio3 } from "./ejercicio3.js";
+import { ejercicio4 } from "./ejercicio4.js";
+import { ejercicio5 } from "./ejercicio5.js";
+import { ejercicio6 } from "./ejercicio6.js";
+import { ejercicio7 } from "./ejercicio7.js";
+import { ejercicio8 } from "./ejercicio8.js";
+import { ejercicio9 } from "./ejercicio9.js";
+import { ejercicio10 } from "./ejercicio10.js";
+import { ejercicio11 } from "./ejercicio11.js";
+import { ejercicio12 } from "./ejercicio12.js";
+import { ejercicio13 } from "./ejercicio13.js";
+import { ejercicio14 } from "./ejercicio14.js";
+import { ejercicio15 } from "./ejercicio15.js";
+import { ejercicio16 } from "./ejercicio16.js";
+import { ejercicio17 } from "./ejercicio17.js";
+import { ejercicio18 } from "./ejercicio18.js";
+import { ejercicio19 } from "./ejercicio19.js";
+import { ejercicio20 } from "./ejercicio20.js";
+
+const ejercicios = [
+  ejercicio1,
+  ejercicio2,
+  ejercicio3,
+  ejercicio4,
+  ejercicio5,
+  ejercicio6,
+  ejercicio7,
+  ejercicio8,
+  ejercicio9,
+  ejercicio10,
+  ejercicio11,
+  ejercicio12,
+  ejercicio13,
+  ejercicio14,
+  ejercicio15,
+  ejercicio16,
+  ejercicio17,
+  ejercicio18,
+  ejercicio19,
+  ejercicio20,
+];
+
+function construirMenu() {
+  let texto = "MENU\n";
+  for (let i = 0; i < ejercicios.length; i++) {
+    texto += `${i + 1}. Ejercicio ${i + 1}\n`;
+  }
+  texto += "0. Salir";
+  return texto;
+}
 
 function ejecutarMenu() {
   let opcion = "";
 
   while (opcion !== "0") {
-    opcion = prompt(
-      "MENU\n" +
-        "1. Ejercicio 1\n" +
-        "2. Ejercicio 2\n" +
-        "3. Ejercicio 3\n" +
-        "4. Ejercicio 4\n" +
-        "5. Ejercicio 5\n" +
-        "6. Ejercicio 6\n" +
-        "7. Ejercicio 7\n" +
-        "8. Ejercicio 8\n" +
-        "9. Ejercicio 9\n" +
-        "10. Ejercicio 10\n" +
-        "11. Ejercicio 11\n" +
-        "12. Ejercicio 12\n" +
-        "13. Ejercicio 13\n" +
-        "14. Ejercicio 14\n" +
-        "15. Ejercicio 15\n" +
-        "16. Ejercicio 16\n" +
-        "17. Ejercicio 17\n" +
-        "18. Ejercicio 18\n" +
-        "19. Ejercicio 19\n" +
-        "20. Ejercicio 20\n" +
-        "0. Salir",
-    );
+    opcion = prompt(construirMenu());
 
-    switch (opcion) {
-      case "1":
-        ejercicio1();
-        break;
-      case "2":{
-  const productos = [
-    { nombre: "Mouse", stock: 3, precio: 50000 },
-    { nombre: "Teclado", stock: 10, precio: 80000 },
-    { nombre: "Monitor", stock: 25, precio: 900000 }
-  ]
+    if (opcion === "0") {
+      break;
+    }
 
-  alert(JSON.stringify(ejercicio2(productos)))
-  break
-}
-        break;
-      case "3":
-        ejercicio3();
-        break;
-      case "4":
-        ejercicio4();
-        break;
-      case "5":
-        ejercicio5();
-        break;
-      case "6":
-        ejercicio6();
-        break;
-      case "7":
-        ejercicio7();
-        break;
-      case "8":
-        ejercicio8();
-        break;
-      case "9":
-        ejercicio9();
-        break;
-      case "10":
-        ejercicio10();
-        break;
-      case "11":
-        ejercicio11();
-        break;
-      case "12":
-        ejercicio12();
-        break;
-      case "13":
-        ejercicio13();
-        break;
-      case "14":
-        ejercicio14();
-        break;
-      case "15":
-        ejercicio15();
-        break;
-      case "16":
-        ejercicio16();
-        break;
-      case "17":
-        ejercicio17();
-        break;
-      case "18":
-        ejercicio18();
-        break;
-      case "19":
-        ejercicio19();
-        break;
-      case "20":
-        ejercicio20();
-        break;
-      case "0":
-        break;
-      default:
-        alert("Opcion invalida");
+    let indice = Number(opcion) - 1;
+
+    if (indice >= 0 && indice < ejercicios.length) {
+      ejercicios[indice]();
+    } else {
+      alert("Opcion invalida");
     }
   }
 }
